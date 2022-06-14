e118890744b90c4c88ac5a8baae478c30a8d6698
AR.LwinHtoo <88469359+lwinhtoo811822@users.noreply.github.com>
2022-06-14 21:53:51 +0630

    Delete ads.js

--- ads.js
+++
@@ -1,2 +0,0 @@
-<script src='//advisedlyremove.com/43/dd/22/43dd229fd14815ca1bd1a8c517de3e11.js' type='text/javascript'/>
-<script src='//advisedlyremove.com/4b/03/07/4b03079a8213eb85e4092bd8fc05283a.js' type='text/javascript'/>
